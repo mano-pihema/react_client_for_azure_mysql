7b2e1bdb14d4cc2f388a86bb6a4b2279eb5e70ec
mano-pihema <manopihema@gmail.com>
2024-05-19 17:25:27 +1200

    set up country

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,13 +1,33 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export function Country({ countryname }) {
+  const { data, isPending } = useQuery({
+    queryKey: ['country'],
+    queryFn: () =>
+      axios
+        .get(`https://mrworld.azurewebsites.net/country/${countryname}`)
+        .then((res) => res.data),
+  })
+  const { Code, Name, Continent, Population, Region } = data
   return (
     <div>
       <h2>Country</h2>
+      {isPending && <div>...Loading</div>}
+      {data && (
+        <div>
+          <p>code:{Code}</p>
+          <p>name:{Name}</p>
+          <p>contient:{Continent}</p>
+          <p>pop:{Population}</p>
+          <p>region:{Region}</p>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 Country.propTypes = {
   countryname: PropTypes.string.isRequired,
-};
+}
