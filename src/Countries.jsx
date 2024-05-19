187a0ba2e151ae8f86c2e8a43e8f465a04009e26
mano-pihema <manopihema@gmail.com>
2024-05-19 14:48:08 +1200

    connect to cloud

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,26 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
-// const API_HOST = import.meta.env.VITE_APP_API_HOST;
-const API_HOST = "https://mr-countries-andrew.azurewebsites.net";
-console.log(API_HOST);
+const API_HOST = import.meta.env.VITE_APP_API_HOST
+// const API_HOST = "https://mr-countries-andrew.azurewebsites.net";
+// console.log(API_HOST);
 
 export function Countries() {
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ["countries"],
+    queryKey: ['countries'],
     queryFn: () => axios.get(`${API_HOST}`).then((res) => res.data),
-  });
+  })
 
   return (
-    <div className="p-8">
-      <h2 className="text-2xl font-bold">All Countries</h2>
-      <div className="mt-4">
+    <div className='p-8'>
+      <h2 className='text-2xl font-bold'>All Countries</h2>
+      <div className='mt-4'>
         {isFetching ? (
-          <div className="text-blue-500">Fetching...</div>
+          <div className='text-blue-500'>Fetching...</div>
         ) : isPending ? (
-          <div className="text-green-500">Loading...</div>
+          <div className='text-green-500'>Loading...</div>
         ) : error ? (
-          <div className="text-red-500">
+          <div className='text-red-500'>
             An error has occurred: {error.message}
           </div>
         ) : (
@@ -36,5 +36,5 @@ export function Countries() {
         )}
       </div>
     </div>
-  );
+  )
 }
