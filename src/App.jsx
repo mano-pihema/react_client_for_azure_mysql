c1c018d07834e74dc4b5272db24ae05390543f8c
mano-pihema <manopihema@gmail.com>
2024-05-19 17:01:24 +1200

    add countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import { ErrorBoundary } from "react-error-boundary";
+import { ErrorBoundary } from 'react-error-boundary'
 import {
   createBrowserRouter,
   useParams,
   RouterProvider,
-} from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Layout } from "./Layout";
-import { Home } from "./Home";
-import { Countries } from "./Countries";
-// import { Population } from "./Population";
-// import { Country } from "./Country";
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Layout } from './Layout'
+import { Home } from './Home'
+import { Countries } from './Countries'
+import { Population } from './Population'
+import { Country } from './Country'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 // ErrorElement component
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
 function ErrorElement({ error }) {
   return (
-    <div role="alert">
+    <div role='alert'>
       <p>Something went wrong:</p>
       <pre>{error.message}</pre>
     </div>
-  );
+  )
 }
 
 ErrorElement.propTypes = {
   error: PropTypes.object.isRequired,
-};
+}
 function App() {
-  // let { countryname, continent } = useParams();
+  let { countryname, continent } = useParams()
   let router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: (
         <ErrorBoundary FallbackComponent={ErrorElement}>
           <Layout />
@@ -43,28 +43,28 @@ function App() {
           element: <Home />,
         },
         {
-          path: "allcountries",
+          path: 'allcountries',
           element: <Countries />,
         },
-        // Uncomment the following lines to add the routes for the Population and Country components
-        // {
-        //   path: "population/:continent",
-        //   element: <Population continent={continent} />,
-        // },
-        // {
-        //   path: "country/:countryname",
-        //   element: <Country countryname={countryname} />,
-        // },
+        //Uncomment the following lines to add the routes for the Population and Country components
+        {
+          path: 'population/:continent',
+          element: <Population continent={continent} />,
+        },
+        {
+          path: 'country/:countryname',
+          element: <Country countryname={countryname} />,
+        },
       ],
     },
-  ]);
+  ])
   return (
-    <div className="h-screen">
+    <div className='h-screen'>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
